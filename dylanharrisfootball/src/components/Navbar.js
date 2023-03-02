909eda879992b51f6747a61aff6b2bc3f49bcf1c
dev-rome <haynesjerome@gmail.com>
2023-03-01 23:32:57 -0500

    change bg color to green

--- dylanharrisfootball/src/components/Navbar.js
+++ dylanharrisfootball/src/components/Navbar.js
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="flex justify-around py-5 bg-black text-white">
+    <nav className="flex justify-around py-5 bg-green-800 text-white">
       <p>Dylan Harris</p>
       <ul className="flex gap-8">
         <li>Home</li>
