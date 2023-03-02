290b172877bed2d899577d9f37308f8bbf9d64fb
dev-rome <haynesjerome@gmail.com>
2023-03-01 22:50:23 -0500

    created navbar component

---
+++ dylanharrisfootball/src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="flex justify-around py-5 bg-black text-white">
+      <p>Dylan Harris</p>
+      <ul className="flex gap-8">
+        <li>Home</li>
+        <li>Stats</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
