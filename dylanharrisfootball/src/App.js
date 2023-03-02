fc4a25d7f6473ca94b3726aa490c0576bec9922e
dev-rome <haynesjerome@gmail.com>
2023-03-01 22:53:02 -0500

    created stats component with placeholder images and text

--- dylanharrisfootball/src/App.js
+++ dylanharrisfootball/src/App.js
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar.js";
+import Header from "./components/Header.js";
+import Stats from "./components/Stats.js";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
---
+++ dylanharrisfootball/src/components/Stats.js
@@ -0,0 +1,48 @@
+function Stats() {
+  return (
+    <section className="py-20">
+      <div className="max-w-6xl m-auto">
+        <div className="flex justify-around mb-16">
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <img
+            className="rounded-xl"
+            src="https://picsum.photos/300/300"
+            alt="football"
+          />
+        </div>
+
+        <div className="flex justify-around mb-16">
+          <img
+            className="rounded-xl"
+            src="https://picsum.photos/300/300"
+            alt="football"
+          />
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+
+        <div className="flex justify-around">
+          <div>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <img
+            className="rounded-xl"
+            src="https://picsum.photos/300/300"
+            alt="football"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Stats;
