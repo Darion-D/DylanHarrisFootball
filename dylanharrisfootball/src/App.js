d51afe13234c3d766deab372f3b4addb16531656
DarionDaire <DarionDaire@gmail.com>
2023-03-02 22:11:43 -0500

    import about component

--- dylanharrisfootball/src/App.js
+++ dylanharrisfootball/src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.js";
 import Header from "./components/Header.js";
 import Stats from "./components/Stats.js";
+import About from "./components/About.js";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <About />
       <Stats />
     </div>
   );
