8e1253c55989f1ad943ff87e41fad60ad137e829
DarionDaire <DarionDaire@gmail.com>
2023-04-05 13:42:19 -0400

    add about me info

--- dylanharrisfootball/src/components/About.js
+++ dylanharrisfootball/src/components/About.js
@@ -1,7 +1,7 @@
 function About() {
     return (
         <section>
-            <p>Its me</p>
+            <p>2x All County | 2x All Division | 2x All Long Island | 2x All State Second Team | 2022 Division 1 MVP | 2x Joe Cipp Award Finalist</p>
         </section>
     )
 };
